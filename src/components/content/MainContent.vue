00b92037b8e78c3de7c98b9a1a52706ec772eaa4
HORKEN <horken.cathay@gmail.com>
2021-09-06 23:48:43 +0800

    Add auto typing method in landing page

--- src/components/content/MainContent.vue
+++ src/components/content/MainContent.vue
@@ -2,8 +2,7 @@
   <div>
     <div class="conference_intro flex flex-col items-center justify-center">
       <img class="w-full max-w-2xl" src="@/assets/images/common/mainLogo.png" alt="JSDC_2021" title="JSDC_2021" />
-      <h1 class="intro_content">JSDC 2021 開發者年會 10/30 將於線上舉行</h1>
-      <div class="sub_content">敬請期待</div>
+      <auto-typing></auto-typing>
     </div>
   </div>
 </template>
@@ -12,13 +11,14 @@
 .conference_intro {
   min-height: calc(100vh - 35px);
 }
-.intro_content,
-.sub_content {
-  @apply text-base;
-  @apply text-white;
-  @apply mt-5;
-  @media screen and (min-width: $tablet-size) {
-    @apply text-3xl;
-  }
-}
 </style>
+
+<script>
+import AutoTyping from '@/components/content/AutoTyping.vue';
+
+export default {
+  components: {
+    AutoTyping,
+  },
+};
+</script>
