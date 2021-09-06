58a66f5c3c45f343e4ca75461df7d385984ab919
HORKEN <horken.cathay@gmail.com>
2021-09-07 01:02:56 +0800

    deploy autotyping style to master

--- src/router/index.js
+++ src/router/index.js
@@ -14,64 +14,64 @@ const routes = [
     },
     component: LandingPage,
   },
-  {
-    path: '/jsdc',
-    name: 'official',
-    beforeEnter() {
-      window.open('https://jsdc.tw/', '_blank');
-    },
-  },
-  {
-    path: '/aboutus',
-    name: 'aboutus',
-    meta: {
-      title: 'JSDC 2021 - 關於我們',
-    },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/speakers',
-    name: 'speaker',
-    meta: {
-      title: 'JSDC 2021 - 講者介紹',
-    },
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/timesheets',
-    name: 'timesheets',
-    meta: {
-      title: 'JSDC 2021 - 大會議程',
-    },
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/sponsors',
-    name: 'sponsors',
-    meta: {
-      title: 'JSDC 2021 - 合作夥伴',
-    },
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/teammember',
-    name: 'teammember',
-    meta: {
-      title: 'JSDC 2021 - 工作團隊',
-    },
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/conduct',
-    name: 'conduct',
-    meta: {
-      title: 'JSDC 2021 - 行為準則',
-    },
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+  // {
+  //   path: '/jsdc',
+  //   name: 'official',
+  //   beforeEnter() {
+  //     window.open('https://jsdc.tw/', '_blank');
+  //   },
+  // },
+  // {
+  //   path: '/aboutus',
+  //   name: 'aboutus',
+  //   meta: {
+  //     title: 'JSDC 2021 - 關於我們',
+  //   },
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/speakers',
+  //   name: 'speaker',
+  //   meta: {
+  //     title: 'JSDC 2021 - 講者介紹',
+  //   },
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/timesheets',
+  //   name: 'timesheets',
+  //   meta: {
+  //     title: 'JSDC 2021 - 大會議程',
+  //   },
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/sponsors',
+  //   name: 'sponsors',
+  //   meta: {
+  //     title: 'JSDC 2021 - 合作夥伴',
+  //   },
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/teammember',
+  //   name: 'teammember',
+  //   meta: {
+  //     title: 'JSDC 2021 - 工作團隊',
+  //   },
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/conduct',
+  //   name: 'conduct',
+  //   meta: {
+  //     title: 'JSDC 2021 - 行為準則',
+  //   },
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
   {
     path: '*',
     name: 'nofound',
