bf6cf2d919ee4ce1b9663ed1818db3acf466b994
HORKEN <horken.cathay@gmail.com>
2021-09-12 02:07:36 +0800

    revert code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="font-sans">
     <div class="hidden">{{appVersion}}</div>
-    <!-- <menu-nav v-if="!['nofound'].includes($route.name)"></menu-nav> -->
+    <menu-nav v-if="!['nofound'].includes($route.name)"></menu-nav>
     <router-view/>
     <footer-side></footer-side>
   </div>
 </template>
 
 <script>
-// import MenuNav from '@/components/layout/MenuNav.vue';
+import MenuNav from '@/components/layout/MenuNav.vue';
 import FooterSide from '@/components/layout/FooterSide.vue';
 
 export default {
   components: {
     FooterSide,
-    // MenuNav,
+    MenuNav,
   },
   computed: {
     appVersion: () => process.env.version,
