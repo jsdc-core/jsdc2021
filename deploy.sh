f9c0c4dfd43119f21ad8624b80c32079ef799d8e
HORKEN <horken.cathay@gmail.com>
2021-09-25 20:37:56 +0800

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
 # build
 npm run build
 # navigate into the build output directory
 cd dist
+
+# get package.json version to show on commit message
+PKG_VERSION=`node -p "require('../package.json').version"`
+commit_message="deploy $PKG_VERSION to gitPage"
+
 # if you are deploying to a custom domain
 echo '2021.jsdc.tw' > CNAME
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "$commit_message"
 
 git push -f git@github.com:jsdc-core/jsdc2021.git master:gh-pages
-echo All done!
+echo $commit_message All done!
 cd -
