dcc783c7e1d171f1fe044ec68edef8efee15f01b
HORKEN <horken.cathay@gmail.com>
2021-09-25 19:48:42 +0800

    team member page done

--- src/views/TeamMember.vue
+++ src/views/TeamMember.vue
@@ -4,7 +4,7 @@
     <div class="member-card flex flex-wrap justify-between">
       <template v-for="pl in memberData">
         <figure class="text-white m-5" :key="pl.name">
-          <img class="rounded-full mb-5" src="@/assets/images/member/logo.jpg" :alt="pl.name" title="JSDC 工作人員">
+          <img class="rounded-full mb-5 w-full" :src="pl.picPath" :alt="pl.name" title="JSDC 工作人員">
           <figcaption class="text-xl">{{pl.name}}</figcaption>
         </figure>
       </template>
@@ -16,13 +16,21 @@
 <script>
 import HelpTooling from '@/components/layout/HelpTooling.vue';
 
+const defaultImage = require('@/assets/images/member/logo.jpg');
+const imgWei = require('@/assets/images/member/weiyuan.jpg');
+const imgMa = require('@/assets/images/member/ma.jpg');
+const imgHorken = require('@/assets/images/member/horken.jpg');
+
 export default {
   data() {
     return {
       redirectUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSehfUy_ZaaByP8ZaDV4YkAf7XvIraTAiBA3kBu5iRP1iC9FDw/viewform',
-      memberData: [{ name: 'Caesar', picPath: '' }, { name: '維元 (Wei)', picPath: '@/assets/images/member/weiyuan.jpg' },
-        { name: 'Ma', picPath: '@/assets/images/member/ma.jpg' }, { name: 'Horken', picPath: '@/assets/images/member/horken.jpg' }],
-      defaultImagePath: '@/assets/images/member/logo.jpg',
+      memberData: [
+        { name: 'Caesar', picPath: defaultImage },
+        { name: '維元 (Wei)', picPath: imgWei },
+        { name: 'Ma', picPath: imgMa },
+        { name: 'Horken', picPath: imgHorken },
+      ],
     };
   },
   components: {
@@ -33,7 +41,7 @@ export default {
 
 <style lang="scss">
 figure {
-  flex: 0 0 25%;
+  flex: 0 0 20%;
   max-width: 20%;
 }
 </style>
