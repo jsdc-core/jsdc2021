06d13360b9e9ee22bdd800afc3a1bda408ef502f
Horken <horkenwong@gmail.com>
2021-10-07 13:25:39 +0800

    update package json file undefined issue (#22)
    Co-authored-by: HORKEN <horken.cathay@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -7,24 +7,24 @@ module.exports = {
     config
       .plugin('html')
       .tap((args) => {
-        args[0].title = process.env.npm_package_title;
+        args[0].title = process.VUE_CLI_SERVICE.pkg.title;
         args[0].meta = [
           { charset: 'utf-8' },
           { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
           { name: 'viewport', content: 'width=device-width,initial-scale=1.0,user-scalable=no' },
-          { name: 'name', content: process.env.npm_package_title },
-          { name: 'description', content: process.env.npm_package_description },
+          { name: 'name', content: process.VUE_CLI_SERVICE.pkg.title },
+          { name: 'description', content: process.VUE_CLI_SERVICE.pkg.description },
           { name: 'keywords', content: 'website, javascript, conference, developers, js, JSDC, 2021' },
           { name: 'author', content: 'JSDC Member' },
           // Facebook meta data
-          { property: 'og:title', content: process.env.npm_package_title },
-          { property: 'og:description', content: process.env.npm_package_description },
-          { property: 'og:site_name', content: process.env.npm_package_title },
+          { property: 'og:title', content: process.VUE_CLI_SERVICE.pkg.title },
+          { property: 'og:description', content: process.VUE_CLI_SERVICE.pkg.description },
+          { property: 'og:site_name', content: process.VUE_CLI_SERVICE.pkg.title },
           // Twitter meta data
           { name: 'twitter:card', content: 'summary' },
           { name: 'ttwitter:creator', content: 'JSDC Member' },
-          { name: 'twitter:title', content: process.env.npm_package_title },
-          { name: 'twitter:card', content: process.env.npm_package_description },
+          { name: 'twitter:title', content: process.VUE_CLI_SERVICE.pkg.title },
+          { name: 'twitter:card', content: process.VUE_CLI_SERVICE.pkg.description },
           { name: 'twitter:site', content: '@jsdc_tw' },
         ];
         return args;
