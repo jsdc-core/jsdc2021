721a34310692e4bbd8eddd1beec8620c993cb199
HORKEN <horken.cathay@gmail.com>
2021-10-17 11:44:58 +0800

    sponser page done

--- src/views/ConfTimesheets.vue
+++ src/views/ConfTimesheets.vue
@@ -39,8 +39,10 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import HelpTooling from '@/components/layout/HelpTooling.vue';
 
+const redirectUrl = ref('https://www.accupass.com/event/2109210508051383682446');
 const timeSheet = [
   {
     time: '08:30 - 08:40',
