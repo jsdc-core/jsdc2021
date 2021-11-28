9cea2c77c8ba90f7df28b0a8fa4166187df6e1ba
HORKEN <horken.cathay@gmail.com>
2021-11-28 21:16:34 +0800

    add purge css path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/assets/styles/*.{css, scss}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
